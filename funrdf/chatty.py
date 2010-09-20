09dc056d6dfddf25da54d4a07c28c3c857d80c33
forman.simon <forman.simon@dccbd1f6-1439-4cd0-cd38-6deb9c818d16>
2010-09-20 03:36:40 +0000

    Add logging to chat UI. Restrict command handler to only accept command from me.

--- funrdf/chatty.py
+++ funrdf/chatty.py
@@ -1,12 +1,31 @@
+import logging
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.xmpp_handlers import CommandHandler
 from google.appengine.ext.webapp.util import run_wsgi_app
 
 
+XMPP_LOG = logging.getLogger("XMPP")
+XMPP_LOG.setLevel(logging.DEBUG)
+
+
 class XMPPHandler(CommandHandler):
 
+    def message_received(self, message):
+        # Guard against random people 'talking' to the app.
+        if message.sender != 'forman.simon@gmail.com':
+            XMPP_LOG.warning('received message from %r' % message.sender)
+            return
+        super(CommandHandler, self).message_received(message)
+
     def hello_command(self, message):
-        message.reply('Bwah! %r' % message.body.lower())
+        reply = 'Bwah! %r' % message.body.lower()
+        XMPP_LOG.debug(reply)
+        message.reply(reply)
+
+    def unhandled_command(self, message):
+        reply = 'Unknown command: %r' % (message.body,)
+        XMPP_LOG.debug(reply)
+        message.reply(reply)
 
 
 application = webapp.WSGIApplication(
