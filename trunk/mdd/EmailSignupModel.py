e601647559e86946c83005059cfd5af94cf909e9
forman.simon <forman.simon@dccbd1f6-1439-4cd0-cd38-6deb9c818d16>
2010-09-24 03:59:20 +0000

    Simplify the model to an initial, easy to grok form.

--- trunk/mdd/EmailSignupModel.py
+++ trunk/mdd/EmailSignupModel.py
@@ -4,82 +4,67 @@ PyModel model for an email signup process, based on the WebModel.py sample.
 
 ### Model
 
+INACTIVE = 'Inactive'
+RUNNING = 'Running'
+
 # State, with initial values
 
-mode = 'Initializing'  # can't call it 'state', that has special meaning
-usersLoggedIn = list()
-userToInt = dict()
+mode = INACTIVE  # can't call it 'state', that has special meaning
+pendingEmails = []
+activeEmails = []
+
 
 # Actions and enabling conditions
 
+
 def Initialize():
   global mode
-  mode = 'Running'
+  mode = RUNNING
 
 def InitializeEnabled():
-  return mode == 'Initializing'
+  return mode == INACTIVE
 
-def Login(user, password):
-  global usersLoggedIn
-  if (password == 'Correct'):
-    usersLoggedIn.append(user)    
-    return 'Success'
-  else:
-    return 'Failure'
 
-def LoginEnabled(user, password):  # arg list must match Action
-  return (mode == 'Running' and user not in usersLoggedIn)
+def Recv(address):
+  global pendingEmails
 
-def Logout(user):
-  global usersLoggedIn
-  usersLoggedIn.remove(user)
+  if address in pendingEmails:
+    pendingEmails.remove(address)
+    activeEmails.append(address)
+    return 'activate'
 
-def LogoutEnabled(user):
-  return (mode == 'Running' and user in usersLoggedIn)
+  pendingEmails.append(address)
+  return 'pending'
 
-def UpdateInt(user, number):
-  global userToInt
-  userToInt[user] = number
+def RecvEnabled(address):
+  return mode == RUNNING and address not in activeEmails
 
-def UpdateIntEnabled(user, number):
-  return (mode == 'Running' and user in usersLoggedIn)
-
-def ReadInt(user):
-  return userToInt.get(user, 0) # return default 0 if user not in dictionary
-
-def ReadIntEnabled(user):
-  return (mode == 'Running' and user in usersLoggedIn)
-
-def Accepting():
-  return not usersLoggedIn
 
 ### Metadata
 
-state = ('mode', 'usersLoggedIn', 'userToInt')
+state = ('mode', 'pendingEmails', 'activeEmails')
 
-actions = (Initialize, Login, Logout, UpdateInt, ReadInt)
+actions = (Initialize, Recv)
 
-enablers = { Initialize:(InitializeEnabled,), Login:(LoginEnabled,), 
-             Logout:(LogoutEnabled,), UpdateInt:(UpdateIntEnabled,), 
-             ReadInt:(ReadIntEnabled,) }
+enablers = {
+  Initialize: (InitializeEnabled,),
+  Recv: (RecvEnabled,),
+  }
 
-cleanup = (Logout,)
+# cleanup = (Logout,)
 
 # default domains
 
 users = ['VinniPuhh', 'OleBrumm']
-passwords = ['Correct', 'Incorrect']
-numbers = [ 1, 2 ]  # different from default 0
 
-domains = { Login: {'user':users, 'password':passwords}, 
-            Logout: {'user':users}, 
-            UpdateInt: {'user':users, 'number':numbers},
-            ReadInt: {'user':users} }
+domains = {
+  Recv: {'address':users},
+  }
 
 # needed for multiple test runs in one session
 
-def Reset():  
-  global mode, usersLoggedin, userToInt
-  mode = 'Initializing'
-  del usersLoggedIn[:]
-  userToInt.clear()
+def Reset():
+  global mode, pendingEmails, activeEmails
+  mode = INACTIVE
+  del pendingEmails[:]
+  del activeEmails[:]
