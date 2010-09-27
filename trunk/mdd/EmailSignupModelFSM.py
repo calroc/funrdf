a38af6df75891922b56a486a940913debd84a7f6
forman.simon <forman.simon@dccbd1f6-1439-4cd0-cd38-6deb9c818d16>
2010-09-27 18:46:40 +0000

    Added error reporting to the process model. (I have no idea yet if this is a good way to do this.)

--- trunk/mdd/EmailSignupModelFSM.py
+++ trunk/mdd/EmailSignupModelFSM.py
@@ -1,27 +1,30 @@
 
 # pma EmailSignupModel
-# 4 states, 3 transitions, 4 accepting states, 1 finished and 0 deadend states
+# 5 states, 8 transitions, 5 accepting states, 0 finished and 0 deadend states
 
 # actions here are just labels, but must be symbols with __name__ attribute
 
 def Initialize(): pass
+def Report(): pass
 def Recv(): pass
+def Err(): pass
 
 # states, key of each state here is its number in graph etc. below
 
 states = {
   0 : {'EmailSignupModel': {'activeEmails': set([]), 'mode': 'Inactive', 'pendingEmails': set([])}},
   1 : {'EmailSignupModel': {'activeEmails': set([]), 'mode': 'Running', 'pendingEmails': set([])}},
-  2 : {'EmailSignupModel': {'activeEmails': set([]), 'mode': 'Running', 'pendingEmails': set(['VinniPuhh'])}},
-  3 : {'EmailSignupModel': {'activeEmails': set(['VinniPuhh']), 'mode': 'Running', 'pendingEmails': set([])}},
+  2 : {'EmailSignupModel': {'activeEmails': set([]), 'mode': 'Error', 'pendingEmails': set([])}},
+  3 : {'EmailSignupModel': {'activeEmails': set([]), 'mode': 'Running', 'pendingEmails': set(['VinniPuhh'])}},
+  4 : {'EmailSignupModel': {'activeEmails': set(['VinniPuhh']), 'mode': 'Running', 'pendingEmails': set([])}},
 }
 
 # initial state, accepting states, frontier states, deadend states
 
 initial = 0
-accepting = [0, 1, 2, 3]
+accepting = [0, 1, 2, 3, 4]
 frontier = []
-finished = [3]
+finished = []
 deadend = []
 runstarts = [0]
 
@@ -29,6 +32,11 @@ runstarts = [0]
 
 graph = (
   (0, (Initialize, (), None), 1),
-  (1, (Recv, ('VinniPuhh', 'sign up'), 'pending'), 2),
-  (2, (Recv, ('VinniPuhh', 'confirm'), 'activate'), 3),
+  (1, (Err, ('VinniPuhh', 'confirm'), None), 2),
+  (1, (Recv, ('VinniPuhh', 'sign up'), 'pending'), 3),
+  (2, (Report, (), None), 0),
+  (3, (Err, ('VinniPuhh', 'sign up'), None), 2),
+  (3, (Recv, ('VinniPuhh', 'confirm'), 'activate'), 4),
+  (4, (Err, ('VinniPuhh', 'sign up'), None), 2),
+  (4, (Err, ('VinniPuhh', 'confirm'), None), 2),
 )
