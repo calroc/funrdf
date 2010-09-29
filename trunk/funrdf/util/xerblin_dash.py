4f9d17f66ac07f5de606f06cf6d33b3e9256f896
forman.simon <forman.simon@dccbd1f6-1439-4cd0-cd38-6deb9c818d16>
2010-09-29 20:41:58 +0000

    Generate simple HTML docs for the command words. These are available under /static/words/<word>.html

--- trunk/funrdf/util/xerblin_dash.py
+++ trunk/funrdf/util/xerblin_dash.py
@@ -1,27 +1,21 @@
+from xerblin.library import words
 from webout import *
 
+title = 'Fun with RDF'
 
-stack_div = DIV('Stack...', Class='stack_div')
-
-dict_div = DIV('Dictionary...', Class='dict_div')
-
-div = DIV(Class='cli_div')
-
-div <= FORM(
-    Class='cli_form',
-    method='post',
-    action='%(action_url)s'
-    ) \
-    <= TABLE(Class='cli_table') \
-        <= (
-            TR(TD(INPUT(name='cli', id='cli', Type='entry'), COLSPAN=2))
-            + TR(
-                TD(INPUT(value='Post Text', Type='button'))
-                +
-                TD(INPUT(value='Run Command', Type='button'))
-               )
-            )
+def word2html(name, doc):
+    head = HEAD(TITLE(name))
+    body = BODY()
+    body <= H1(name)
+    if doc:
+        doc = ' '.join(doc.split())
+    else:
+        doc = 'No documentation available.'
+    body <= doc
+    return str(HTML(head + body))
 
 
-print DIV(stack_div + dict_div + div, Class='shell_div')
+for name, word in words:
+    with open(name + '.html', 'w') as f:
+        print >> f, word2html(name, word.__doc__)
 
