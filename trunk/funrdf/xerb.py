56dd23bf3850fb67de7e28ac4df7b52028bc8df1
forman.simon <forman.simon@dccbd1f6-1439-4cd0-cd38-6deb9c818d16>
2010-09-29 21:04:43 +0000

    Creating a Xerblin interface.

---
+++ trunk/funrdf/xerb.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import util
+from util.homepage import html
+
+
+class MainHandler(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write(html)
+
+
+def main():
+    application = webapp.WSGIApplication(
+        [
+            ('/', MainHandler),
+            ],
+        debug=True,
+        )
+    util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
